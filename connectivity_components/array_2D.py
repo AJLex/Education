aae689618d6b681ea224835b05f6bda7f74f7788
AJLex <ashuliatyev@gmail.com>
2017-12-13 19:34:25 +0300

    4 'if' -> 1 'for' and fixed tests

--- connectivity_components/array_2D.py
+++ connectivity_components/array_2D.py
@@ -1,33 +1,25 @@
-def f(data, start, graph=None):
+def f(data, start, visited, graph=None):
     if data and start:
         x, y = start[0], start[1]
         if  graph is None:
             graph = [start]
-            data.remove(start)
-        if [x+1, y] in data:
-            graph.append([x+1, y])
-            data.remove([x+1, y])
-            f(data, [x+1, y], graph)
-        if [x, y+1] in data:
-            graph.append([x, y+1])
-            data.remove([x, y+1])
-            f(data, [x, y+1], graph)
-        if [x-1, y] in data:
-            graph.append([x-1, y])
-            data.remove([x-1, y])
-            f(data, [x-1, y], graph)
-        if [x, y-1] in data:
-            graph.append([x, y-1])
-            data.remove([x, y-1])
-            f(data, [x, y-1], graph)
-    return data, graph
+            visited.append(start)
+        for a, b in [[x+1, y], [x-1, y], [x, y+1], [x, y-1]]:
+            if [a, b] in data and [a, b] not in visited:
+                graph.append([a, b])
+                visited.append([a, b])
+                f(data, [a, b], visited, graph)
+    return graph, visited
 
 
 if __name__ == '__main__':
     connect_comp = []
+    visited = []
     data = [[0, 0], [0, 1], [1, 1], [2, 1], [3, 1], [3, 0], [5, 5], [4, 5], [6, 5]]
     for vertex in data:
-        result = f(data, vertex)
-        data = result[0]
-        connect_comp.append(result[1])
+        if vertex not in visited:
+            result = f(data, vertex, visited)
+            visited = result[1]
+            connect_comp.append(result[0])
+            print(result[0])
     print('Число компонент связности:\n', len(connect_comp))
